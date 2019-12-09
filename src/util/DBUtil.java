d878993dec8943c13d52716733a7a191d6eebef4
Tanyiqu <1953649096@qq.com>
2019-12-09 21:19:31 +0800

    初步测试

---
+++ src/util/DBUtil.java
@@ -0,0 +1,127 @@
+package util;
+
+import java.sql.*;
+
+public class DBUtil {
+
+	public static final String DB = "ZYGL";
+	public static final String USER="fw";
+	public static final String PASSWORD="108412696";
+	public static final String URL = "jdbc:sqlserver://127.0.0.1:1433;databaseName=" + DB + ";user=" + USER + ";password=" + PASSWORD;
+	protected static Statement s=null;
+	protected static ResultSet rs=null;
+	protected static Connection conn=null;
+
+	/**
+	 * 创建数据库链接
+	 * @return 数据库连接
+	 */
+	public static synchronized Connection getConnection(){
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn=DriverManager.getConnection(URL);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	/**
+	 **执行insert update delete语句
+	 * @param sql
+	 * @return 执行结果 int
+	 */
+	public static int executeUpdate(String sql){
+		int result=0;
+		try {
+			s=getConnection().createStatement();
+			result=s.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	/**
+	 **执行select语句
+	 * @param sql
+	 * @return result结果集
+	 */
+	public static ResultSet executeQuery(String sql){
+		try {
+			s=getConnection().createStatement();
+			rs=s.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+
+	/**
+	 **执行动态sql语句
+	 * @param sql
+	 * @return PreparedStatement
+	 */
+	public static PreparedStatement executePreparedStatment(String sql){
+		PreparedStatement ps=null;
+		try {
+			ps=getConnection().prepareStatement(sql);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ps;
+	}
+
+
+	/**
+	 **事务回滚
+	 */
+	public static void rollback(){
+		try {
+			getConnection().rollback();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 **关闭连接对象
+	 */
+	public static void close(){
+		try {
+			if(rs!=null)
+				rs.close();
+			if(s!=null)
+				s.close();
+			if(conn!=null)
+				conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 **检测密码是否正确
+	 * @param sql sql
+	 * @return 是否正确
+	 */
+	public static boolean checkPasswd(String sql) {
+		ResultSet rs = DBUtil.executeQuery(sql);
+		String passwd = null;
+		try {
+			rs.next();
+			passwd = rs.getString("Passwd");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return (passwd != null);
+	}
+
+}
